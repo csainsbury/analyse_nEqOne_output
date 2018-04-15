992b92695116d1c960abc76ee7f0777b97dcc735
chris sainsbury <chris@sainsbury.im>
2018-04-15 09:24:17 +0100

    .

--- analyse_output.R
+++ analyse_output.R
@@ -6,9 +6,13 @@ bins_in_testPeriod = 12
 # load drug data for all IDs from test set
 drugData <- read.csv("./pythonOutput/X_test_drugs.csv", header = FALSE)
 # trim to runinData only
+
 # load lookup to interpret drug data
 drugLookup <- read.csv("./pythonOutput/lookup.csv")
-drug
+drugLookupDT <- data.table(drugLookup)
+
+# number for metformin alone
+interestNumber <- drugLookupDT[vectorWords == "Metformin_"]$vectorNumbers
 
 
 
