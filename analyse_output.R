c603a8a947cf3a5defcb3199173f41705e05c6df
chris sainsbury <chris@sainsbury.im>
2018-04-15 10:17:32 +0100

    .

--- analyse_output.R
+++ analyse_output.R
@@ -2,17 +2,23 @@ library(data.table)
 
 total_n_bins = 60
 bins_in_testPeriod = 12
+bins_in_runin_testPeriod = 12
+
+interestDrug = "Metformin_"
 
 # load drug data for all IDs from test set
 drugData <- read.csv("./pythonOutput/X_test_drugs.csv", header = FALSE)
 # trim to runinData only
+runinDrugData <- drug_data[, 1:(ncol(drugData) - bins_in_testPeriod)]
+# extract runin_testPeriod only
+testExtract <- runinDrugData[, ((ncol(runinDrugData) - bins_in_runin_testPeriod) + 1): ncol(runinDrugData)]
 
 # load lookup to interpret drug data
 drugLookup <- read.csv("./pythonOutput/lookup.csv")
 drugLookupDT <- data.table(drugLookup)
 
 # number for metformin alone
-interestNumber <- drugLookupDT[vectorWords == "Metformin_"]$vectorNumbers
+interestNumber <- drugLookupDT[vectorWords == interestDrug]$vectorNumbers
 
 
 
