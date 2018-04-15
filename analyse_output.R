061dfac67a6276acbc5667c98e557f4daf1235b3
chris sainsbury <chris@sainsbury.im>
2018-04-15 15:55:17 +0100

    .

--- analyse_output.R
+++ analyse_output.R
@@ -38,12 +38,41 @@ report_interestDrug_noChange <- ifelse(report_sameDrug == 1 & report_drugOfInter
 
 ## load in predictions
 therapyArray <- read.csv("./pythonOutput/therapyArray.csv", header = FALSE)
+vectorWordsAsCharacter <- as.character(drugLookupDT$vectorWords)
+
 for (j in seq(1, nrow(therapyArray), 1)) {
-  therapyArray$drugNames[j] <- drugLookupDT[vectorNumbers == therapyArray$V1[j]]$vectorWords
+  therapyArray$drugNames[j] <- vectorWordsAsCharacter[drugLookupDT$vectorNumbers == therapyArray$V1[j]]
 }
 
-pred0 <- read.csv("./pythonOutput/y_pred_asNumber_combinationNumber_0.csv", header = FALSE)
-pred1 <- read.csv("./pythonOutput/y_pred_asNumber_combinationNumber_1.csv", header = FALSE)
+# generate analysis frame
+# predictions, starting hba1c / sbp etc
+analysisFrame <- as.data.frame(matrix(0, nrow = nrow(drugData), ncol = nrow(therapyArray)))
+colnames(analysisFrame) <- therapyArray$drugNames
+
+# first add drug combinations as will be different between experiments
+for (ii in seq(1, nrow(therapyArray), 1)) {
+  
+  drugExperiment_n <- (ii - 1) # because from python start at 0
+  
+  pathToRead <- paste("./pythonOutput/y_pred_asNumber_combinationNumber_",drugExperiment_n, ".csv", sep = "")
+  
+  analysisFrame[, ii] <- read.csv(pathToRead, header = FALSE)
+  
+}
+
+# then add consistent parameters
+bmiData <- read.csv("./pythonOutput/decoded_Xtest_bmi.csv", header = FALSE)
+hba1cData <- read.csv("./pythonOutput/decoded_Xtest_hba1c.csv", header = FALSE)
+sbpData <- read.csv("./pythonOutput/decoded_Xtest_sbp.csv", header = FALSE)
+# ageData <- read.csv("./pythonOutput/X_test_age.csv", header = FALSE)
+
+analysisFrame$bmi <- bmiData[, (total_n_bins - bins_in_testPeriod)]
+analysisFrame$hba1c <- hba1cData[, (total_n_bins - bins_in_testPeriod)]
+analysisFrame$sbp <- sbpData[, (total_n_bins - bins_in_testPeriod)]
+
+###
+# subset analysisFrame to IDs of interest (ie on the interest drug during runin)
+interestAnalysisFrame <- analysisFrame[report_interestDrug_noChange == 1, ]
 
 
 
