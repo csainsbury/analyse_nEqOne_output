b9599b552afe63ce932d154d7864722aa13d57b1
chris sainsbury <chris@sainsbury.im>
2018-04-15 09:22:22 +0100

    first commit

---
+++ analyse_output.R
@@ -0,0 +1,20 @@
+library(data.table)
+
+total_n_bins = 60
+bins_in_testPeriod = 12
+
+# load drug data for all IDs from test set
+drugData <- read.csv("./pythonOutput/X_test_drugs.csv", header = FALSE)
+# trim to runinData only
+# load lookup to interpret drug data
+drugLookup <- read.csv("./pythonOutput/lookup.csv")
+drug
+
+
+
+
+# find IDs on metformin alone in last year of runin
+
+# find predictions for each of the 2nd line agents
+
+# find highest prediction
