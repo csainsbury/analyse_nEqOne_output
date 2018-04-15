0e80ec45c56214172a01b0fb7f931ec1a26cf75d
chris sainsbury <chris@sainsbury.im>
2018-04-15 11:31:02 +0100

    .
    very rudimentary progress

--- analyse_output.R
+++ analyse_output.R
@@ -9,7 +9,7 @@ interestDrug = "Metformin_"
 # load drug data for all IDs from test set
 drugData <- read.csv("./pythonOutput/X_test_drugs.csv", header = FALSE)
 # trim to runinData only
-runinDrugData <- drug_data[, 1:(ncol(drugData) - bins_in_testPeriod)]
+runinDrugData <- drugData[, 1:(ncol(drugData) - bins_in_testPeriod)]
 # extract runin_testPeriod only
 testExtract <- runinDrugData[, ((ncol(runinDrugData) - bins_in_runin_testPeriod) + 1): ncol(runinDrugData)]
 
@@ -19,17 +19,34 @@ drugLookupDT <- data.table(drugLookup)
 
 # number for metformin alone
 interestNumber <- drugLookupDT[vectorWords == interestDrug]$vectorNumbers
+
+# flags for same drug throughout final year and interest drug
 report_sameDrug <- rep(0, nrow(testExtract))
 report_drugOfInterest <- rep(0, nrow(testExtract))
 
 for (i in seq(1, nrow(testExtract), 1)) {
   
-  rowValues <- as.numeric(testExtract[i, ])
+  if(i%%400 == 0){print(i)}
   
+  rowValues <- as.numeric(testExtract[i, ])
+  report_sameDrug[i] <- ifelse(sum(diff(rowValues)) == 0, 1, 0)
+  report_drugOfInterest[i] <- ifelse(rowValues[1] == interestNumber, 1, 0)
   
 }
 
-# are all drugs in the test period the same
+report_interestDrug_noChange <- ifelse(report_sameDrug == 1 & report_drugOfInterest == 1, 1, 0)
+
+## load in predictions
+therapyArray <- read.csv("./pythonOutput/therapyArray.csv", header = FALSE)
+for (j in seq(1, nrow(therapyArray), 1)) {
+  therapyArray$drugNames[j] <- drugLookupDT[vectorNumbers == therapyArray$V1[j]]$vectorWords
+}
+
+pred0 <- read.csv("./pythonOutput/y_pred_asNumber_combinationNumber_0.csv", header = FALSE)
+pred1 <- read.csv("./pythonOutput/y_pred_asNumber_combinationNumber_1.csv", header = FALSE)
+
+
+
 
 # find IDs where runin test period all the interest drug
 
