d28b985fc05de598e7c1c89407af179f8d07fc43
chris sainsbury <chris@sainsbury.im>
2018-05-11 16:44:26 +0100

    add support for multiple analysis runs
    .

--- analyse_output.R
+++ analyse_output.R
@@ -2,19 +2,23 @@ library(data.table)
 
 total_n_bins = 60
 bins_in_testPeriod = 12
-bins_in_runin_testPeriod = 12
+bins_in_runin_testPeriod = 36
 
 interestDrug = "Metformin_"
 
+# load number of loops
+nLoops <- read.csv("~/R/_workingDirectory/analyse_nEqOne_output/pythonOutput/numberOfLoops.csv", header = FALSE)
+nLoops_integer <- nLoops[1, 1]
+
 # load drug data for all IDs from test set
-drugData <- read.csv("./pythonOutput/X_test_drugs.csv", header = FALSE)
+drugData <- read.csv("~/R/_workingDirectory/analyse_nEqOne_output/pythonOutput/X_test_drugs.csv", header = FALSE)
 # trim to runinData only
 runinDrugData <- drugData[, 1:(ncol(drugData) - bins_in_testPeriod)]
 # extract runin_testPeriod only
 testExtract <- runinDrugData[, ((ncol(runinDrugData) - bins_in_runin_testPeriod) + 1): ncol(runinDrugData)]
 
 # load lookup to interpret drug data
-drugLookup <- read.csv("./pythonOutput/lookup.csv")
+drugLookup <- read.csv("~/R/_workingDirectory/analyse_nEqOne_output/pythonOutput/lookup.csv")
 drugLookupDT <- data.table(drugLookup)
 
 # number for metformin alone
@@ -37,7 +41,7 @@ for (i in seq(1, nrow(testExtract), 1)) {
 report_interestDrug_noChange <- ifelse(report_sameDrug == 1 & report_drugOfInterest == 1, 1, 0)
 
 ## load in predictions
-therapyArray <- read.csv("./pythonOutput/therapyArray.csv", header = FALSE)
+therapyArray <- read.csv("~/R/_workingDirectory/analyse_nEqOne_output/pythonOutput/therapyArray.csv", header = FALSE)
 vectorWordsAsCharacter <- as.character(drugLookupDT$vectorWords)
 
 for (j in seq(1, nrow(therapyArray), 1)) {
@@ -54,16 +58,16 @@ for (ii in seq(1, nrow(therapyArray), 1)) {
   
   drugExperiment_n <- (ii - 1) # because from python start at 0
   
-  pathToRead <- paste("./pythonOutput/y_pred_asNumber_combinationNumber_",drugExperiment_n, ".csv", sep = "")
+  pathToRead <- paste("~/R/_workingDirectory/analyse_nEqOne_output/pythonOutput/y_pred_asNumber_combinationNumber_",drugExperiment_n, ".csv", sep = "")
   
   analysisFrame[, ii] <- read.csv(pathToRead, header = FALSE)
   
 }
 
 # then add consistent parameters
-bmiData <- read.csv("./pythonOutput/decoded_Xtest_bmi.csv", header = FALSE)
-hba1cData <- read.csv("./pythonOutput/decoded_Xtest_hba1c.csv", header = FALSE)
-sbpData <- read.csv("./pythonOutput/decoded_Xtest_sbp.csv", header = FALSE)
+bmiData <- read.csv("~/R/_workingDirectory/analyse_nEqOne_output/pythonOutput/decoded_Xtest_bmi.csv", header = FALSE)
+hba1cData <- read.csv("~/R/_workingDirectory/analyse_nEqOne_output/pythonOutput/decoded_Xtest_hba1c.csv", header = FALSE)
+sbpData <- read.csv("~/R/_workingDirectory/analyse_nEqOne_output/pythonOutput/decoded_Xtest_sbp.csv", header = FALSE)
 # ageData <- read.csv("./pythonOutput/X_test_age.csv", header = FALSE)
 
 analysisFrame$bmi <- bmiData[, (total_n_bins - bins_in_testPeriod)]
@@ -73,6 +77,17 @@ analysisFrame$sbp <- sbpData[, (total_n_bins - bins_in_testPeriod)]
 ###
 # subset analysisFrame to IDs of interest (ie on the interest drug during runin)
 interestAnalysisFrame <- analysisFrame[report_interestDrug_noChange == 1, ]
+interestAnalysisFrame$maxNames = names(interestAnalysisFrame[, 1:7])[apply(interestAnalysisFrame[, 1:7], 1, which.max)]
 
+plotFrame <- subset(interestAnalysisFrame, sbp > 0 & bmi > 0)
+plot(plotFrame$hba1c, plotFrame$sbp / plotFrame$bmi, ylim = c(2, 8), col = ifelse(plotFrame$maxNames == "DPP4_Metformin_", 2, 
+                                                                                  ifelse(plotFrame$maxNames == "humanBDmixInsulin_Metformin_", 3, 
+                                                                                         ifelse(plotFrame$maxNames == "Metformin_", 4, 
+                                                                                                ifelse(plotFrame$maxNames == "Metformin_SGLT2_", 5, 
+                                                                                                       ifelse(plotFrame$maxNames == "GLP1_Metformin_", 6, 1))))), pch = 16, cex = 2, xlim = c(30, 130))
+abline(lm(plotFrame$sbp / plotFrame$bmi ~ plotFrame$hba1c))
+
+print(table(plotFrame$maxNames))
 # observations:
 # GLP1 favoured. need to add SU alone and an insulin option - basal and basal bolus
+
